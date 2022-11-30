9d7b6b6893130ade93c4b5a660f55c964b562c35
OgabekDev <ogabekdev@gmail.com>
2022-11-30 18:28:58 +0500

    All Done

--- Task 4/ContentView.swift
+++ Task 4/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
+extension UIScreen {
+    static let width = UIScreen.main.bounds.size.width
+    static let height = UIScreen.main.bounds.size.height
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("1")
+                    .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("2")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                    
+                    Text("3")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: UIScreen.width / 2)
+            }.frame(maxHeight: UIScreen.height / 5 * 2)
+            
+            HStack {
+                Text("4")
+                    .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                
+                Text("5")
+                    .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                
+                Text("6")
+                    .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                
+                Text("7")
+                    .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+            }.frame(maxHeight: UIScreen.height / 5)
+            
+            HStack {
+                
+                HStack {
+                    VStack {
+                        Text("8")
+                            .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                        Text("9")
+                            .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Text("10")
+                        .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                }.frame(maxHeight: UIScreen.height / 2)
+                
+                VStack {
+                    Text("11")
+                        .frame(maxWidth: UIScreen.width / 2, maxHeight: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }.frame(maxHeight: UIScreen.height / 2)
+                
+            }.frame(maxHeight: UIScreen.height / 5 * 2)
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .background(Color.black)
     }
 }
 
